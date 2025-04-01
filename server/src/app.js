04a0d45a6953d19f24066933f89d972599afa0ec
m-h-thomas <mhthomas.8193@gmail.com>
2025-04-01 11:08:29 -0600

    configured server to pull from database using knex

--- server/src/app.js
+++ server/src/app.js
@@ -2,4 +2,21 @@
 const express = require('express');
 const app = express();
 const PORT = 8081;
+const knex = require('knex')(require('../knexfile.js')['development']);
 
+app.get('/', (req, res) => {
+  res.send('Application up and running.')
+})
+
+app.get('/items', (req, res) => {
+  knex('item')
+    .select('*')
+    .then(items => {
+      var itemNames = items.map(item => item.item_name)
+      res.json(itemNames);
+    })
+})
+
+app.listen(PORT, () => {
+  console.log(`Express server is listening on port:${PORT}.`)
+})
